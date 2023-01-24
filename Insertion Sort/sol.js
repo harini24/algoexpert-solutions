6965b6b2f790b480bdb974ae8688f2066b9c49c0
harini24 <harinisiva1997@gmail.com>
2023-01-24 23:14:18 +0530

    insertion sort sol

---
+++ Insertion Sort/sol.js
@@ -0,0 +1,13 @@
+function insertionSort(array) {
+  // Write your code here.
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      let temp = array[j];
+      array[j] = array[j - 1];
+      array[j - 1] = temp;
+      j--;
+    }
+  }
+  return array;
+}
